2f2af3829dbe70d74228b8553b6ee87fdd0dc260
Mr. K4US4R <ahkausar2@gmail.com>
2022-06-28 00:29:30 +0600

    Add files via upload

---
+++ Bpass.py
@@ -0,0 +1,39 @@
+import os,time
+
+def fuck():
+	os.system('clear')
+	fack = ("""
+
+\033[96;1m██████  ██    ██ ██████   █████  ███████ ███████ 
+\033[96;1m██   ██  ██  ██  ██   ██ ██   ██ ██      ██      
+\033[96;1m██████    ████   ██████  ███████ ███████ ███████ 
+\033[96;1m██   ██    ██    ██      ██   ██      ██      ██ 
+\033[96;1m██████     ██    ██      ██   ██ ███████ ███████ 
+\033[96;1mFUCK YOUR PAID TOOLS
+\033[96;1m[+] AJIJUL HASAN JIBON [+]
+\033[96;1m[+] SPAMMER BANGLADESHI [+]
+\033[92;1m[It's JIBON] BROH🤓
+\033[96;1mFB🆔m./Ajijul.Hasan.Jibon.Official75
+""")
+	print(fack)
+	print("\033[96;1m [1] SSB VR: 7.6.8 \033[96;1m[BYPASS]		")
+	print("\033[93;1m [2] SSB VR: 8.0.0 \033[96;1m[BYPASS]	")
+	print("\033[93;1m [3] James VR:0.3\033[96;1m[BYPASS By Jibon]	")
+	print("\033[93;1m [4]Fcpromax\033[96;1m[Crack]	")
+	print("")
+	xx= input(" [•] CHOICE : ")
+	if xx in ["1","01"]:
+		time.sleep(1.5)
+		os.system('python s.py')
+	if xx in ["2","02"]:
+		time.sleep(1.5)
+		os.system('python s1.py')
+	if xx in ["3","03"]:
+		time.sleep(1.5)
+	os.system('python j.py')
+	if xx in ["4","04"]:
+		time.sleep(1.5)
+	os.system('python fcpromax.py')
+	
+
+fuck()
---
+++ Fuck_FC_PRO.py
@@ -0,0 +1,5 @@
+import os
+if __name__ == "__main__":
+	os.system("git pull")
+	__import__("fcpromax").azimvau69()
+
