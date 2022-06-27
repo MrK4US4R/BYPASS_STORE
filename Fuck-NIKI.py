daaf545885903d67575a06fc61bc27e4440ee9fd
Mr. K4US4R <ahkausar2@gmail.com>
2022-06-28 00:27:20 +0600

    Add files via upload

---
+++ Fuck-NIKI.py
@@ -0,0 +1,19 @@
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+
+import requests,time
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    print('\n\n\n\n\n FUCK NIKI IN PROGRESS...!')
+    time.sleep(2)
+    os.system('clear')
+    print('\n\n\n\n\n BYPASS BY 💁 MrKAUSAR Respect Bro...!')
+import os, sys
+try:
+    __import__("wow5").__niki____()
+except Exception as e:
+    exit(str(e))
---
+++ Lovely.py
@@ -0,0 +1,5 @@
+import os, sys
+try:
+    __import__("wow3").__niki____()
+except Exception as e:
+    exit(str(e))
